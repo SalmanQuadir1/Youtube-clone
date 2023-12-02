fa1d9c7959484c91579aa96b74084f135ca7b50c
salman <najarsalman4@gmail.com>
2023-12-02 20:59:24 +0530

    store

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { toggleMenu } from '../utils/appSlice';
 
 const Head = () => {
+    const dispatch = useDispatch();
     return (
         <div className='grid grid-flow-col p-4  my-1 shadow-lg'>
             <div className='flex col-span-1'>
-                <img className='h-14' src="https://static.vecteezy.com/system/resources/previews/021/190/402/original/hamburger-menu-filled-icon-in-transparent-background-basic-app-and-web-ui-bold-line-icon-eps10-free-vector.jpg" alt="hamburger" />
+                <img className='h-14 cursor-pointer' onClick={() => dispatch(toggleMenu())} src="https://static.vecteezy.com/system/resources/previews/021/190/402/original/hamburger-menu-filled-icon-in-transparent-background-basic-app-and-web-ui-bold-line-icon-eps10-free-vector.jpg" alt="hamburger" />
                 <img className='h-14' src="https://t3.ftcdn.net/jpg/03/00/38/90/360_F_300389025_b5hgHpjDprTySl8loTqJRMipySb1rO0I.jpg" alt="" />
 
             </div>
