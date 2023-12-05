fc24b6d5c867f179185cef5706b2891da8eef05b
salman <najarsalman4@gmail.com>
2023-12-05 10:25:59 +0530

    new const

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,4 @@
 const API_CREDENTIALS = "AIzaSyDino5j_AGesLmB1G09ZolBN05tE9xRdr8";
 export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + API_CREDENTIALS;
+
+export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
